50f4e256e5f8bb248ed57c2fe8c796db6525e33b
mafer <bgmarifer99@gmail.com>
2026-04-17 12:23:30 -0600

    Haciendo una prueba integrando docs

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 //Imports del navigator
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { StyleSheet, Text, TouchableOpacity, View, Linking } from 'react-native';
+import { useCallback } from 'react';
 
 //Se importan todas las screens que vamos a utilizar
 import LoginScreen from './screens/login';
@@ -10,6 +12,34 @@ import Profile from './screens/profile';
 import Test from './screens/test'
 
 const Stack = createNativeStackNavigator();
+
+//Generando componente para probar GoogleDocs 
+const Prueba = () => {
+  const handleOpenDocument = useCallback(async () => {
+    //El ID unico del documento que debería generarse al almacenarlo dentro de Google Drive
+    const fileId = '1k92MpDpuXNjmnd-K8ulXkRowfwbdAcNndvJFEVJRXMM';
+    //Formamos el path completo para abrir el documento
+    const url = `https://docs.google.com/document/d/${fileId}/edit?usp=sharing&force_shell=1`;
+    const url2 = `https://www.google.com/url?q=${url}`;
+
+    try {
+      const available = await Linking.canOpenURL(url);
+      if(available){
+        await Linking.openURL(url);
+      }
+    } catch(error){
+      console.log("Se ha producido un error al abrir el documento");
+    }
+  })
+
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity style={styles.button} onPress={() => {handleOpenDocument()}}>
+        <Text style={{color: 'white', fontWeight: 'bold'}}>Probando</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
 
 export default function App() {
   return (
@@ -47,3 +77,23 @@ export default function App() {
     </NavigationContainer>
   );
 }
+
+//Estilos solo para poder probar la integració con GoogleDocs
+const styles = StyleSheet.create({
+  container: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    flex: 1,
+    backgroundColor: 'black'
+  },
+  button: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '40%',
+    height: '10%',
+    backgroundColor: 'red',
+    borderRadius: 10,
+  }
+});
